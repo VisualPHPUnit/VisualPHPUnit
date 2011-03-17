dd7ad120e0edcdffccaae3bb4ccf8d44eb96f5ad
Nick Sinopoli <NSinopoli@gmail.com>
2011-03-17 12:02:49 -0700

    Protect against unauthorized file access.

--- Main/index.php
+++ Main/index.php
@@ -6,35 +6,40 @@ function_exists($action) ? $action() : error();
 exit(0);
 
 function main() {
-	// include_once($_SERVER['DOCUMENT_ROOT'].'/ScarletFinal/classes/Template.php');
-	
-	// $template = new Template('main.tpl/');
-	
-	include('header.php');
-	
-	// $template->compile('main.tpl');
-	require_once '../Sandbox.php';
-	include '../PHPUnit.php';
-		
-		
-	if(isset($_GET['path']) && is_dir($_GET['path'])) {
-		$suite = (isset($_GET['test'])) ? $_GET['test'] : '';
-		
-		$phpunit = new PHPUnit($_GET['path'], $suite);
-		
-		chdir($_GET['path']);
-	} else {
-		throw new Exception("Could not find the specified tests directory: <strong>".$_GET['path'].'</strong>', 1);
-	}
-		
-	$results = $phpunit->run();
-	// print_r($results);
-	echo $phpunit->toHTML($results);
-	
-	include('footer.php');
-	
-	
-	// include('main.php');
+    // include_once($_SERVER['DOCUMENT_ROOT'].'/ScarletFinal/classes/Template.php');
+    
+    // $template = new Template('main.tpl/');
+    
+    include('header.php');
+    
+    // $template->compile('main.tpl');
+    require_once '../Sandbox.php';
+    include '../PHPUnit.php';
+
+    // Protect against unauthorized file access. 
+    $path = str_replace('../', '', $_GET['path']); 
+    $_GET['path'] = realpath(dirname(__FILE__) . '/../../' . $path); 	
+    if (isset($_GET['path']) && is_dir($_GET['path'])) 
+    {
+        $suite = (isset($_GET['test'])) ? $_GET['test'] : '';
+        
+        $phpunit = new PHPUnit($_GET['path'], $suite);
+        
+        chdir($_GET['path']);
+    } 
+    else 
+    {
+        throw new Exception("Could not find the specified tests directory: <strong>".$_GET['path'].'</strong>', 1);
+    }
+            
+    $results = $phpunit->run();
+    // print_r($results);
+    echo $phpunit->toHTML($results);
+    
+    include('footer.php');
+    
+    
+    // include('main.php');
 }
 
 ?>
