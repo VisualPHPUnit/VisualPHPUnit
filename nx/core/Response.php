0323304a4af7d829a07d2e179c63a00e4c77679e
Johannes Frandsen <localgod@heaven.dk>
2015-05-31 12:21:29 +0200

    #119 Fix to scrutinizer-ci issues

--- nx/core/Response.php
+++ nx/core/Response.php
@@ -1,29 +1,28 @@
 <?php
-
 namespace nx\core;
 
 /**
  * The Response class is used to render an HTTP response.
  *
- * @author    Nick Sinopoli <NSinopoli@gmail.com>
+ * @author Nick Sinopoli <NSinopoli@gmail.com>
  * @copyright 2011-2012 Nick Sinopoli
- * @license   http://opensource.org/licenses/BSD-3-Clause The BSD License
+ * @license http://opensource.org/licenses/BSD-3-Clause The BSD License
  */
 class Response
 {
 
-   /**
-    * The configuration settings.
-    *
-    * @var array
-    */
+    /**
+     * The configuration settings.
+     *
+     * @var array
+     */
     protected $config = array();
 
-   /**
-    *  The HTTP status codes.
-    *
-    *  @var array
-    */
+    /**
+     * The HTTP status codes.
+     *
+     * @var array
+     */
     protected $statuses = array(
         100 => 'Continue',
         101 => 'Switching Protocols',
@@ -66,30 +65,31 @@ class Response
         504 => 'Gateway Time-out'
     );
 
-   /**
-    * Sets the configuration options.
-    *
-    * Possible keys include the following:
-    *
-    * * 'buffer_size' - The number of bytes each chunk of output should contain
-    *
-    * @param array $config    The configuration options.
-    * @return void
-    */
+    /**
+     * Sets the configuration options.
+     *
+     * Possible keys include the following:
+     *
+     * * 'buffer_size' - The number of bytes each chunk of output should contain
+     *
+     * @param array $config
+     *            The configuration options.
+     */
     public function __construct(array $config = array())
     {
         $defaults = array(
-            'buffer_size'  => 8192
+            'buffer_size' => 8192
         );
         $this->config = $config + $defaults;
     }
 
-   /**
-    * Converts an integer status to a well-formed HTTP status header.
-    *
-    * @param int $code    The integer associated with the HTTP status.
-    * @return string
-    */
+    /**
+     * Converts an integer status to a well-formed HTTP status header.
+     *
+     * @param int $code
+     *            The integer associated with the HTTP status.
+     * @return string
+     */
     protected function convertStatus($code)
     {
         if (isset($this->statuses[$code])) {
@@ -98,24 +98,27 @@ class Response
         return "HTTP/1.1 200 OK";
     }
 
-   /**
-    * Parses a response.
-    *
-    * @param mixed $response    The response to be parsed.  Can be an array
-    *                           containing 'body', 'headers', and/or 'status'
-    *                           keys, or a string which will be used as the
-    *                           body of the response.  Note that the headers
-    *                           must be well-formed HTTP headers, and the
-    *                           status must be an integer (i.e., the one
-    *                           associated with the HTTP status code).
-    * @return array
-    */
+    /**
+     * Parses a response.
+     *
+     * @param mixed $response
+     *            The response to be parsed. Can be an array
+     *            containing 'body', 'headers', and/or 'status'
+     *            keys, or a string which will be used as the
+     *            body of the response. Note that the headers
+     *            must be well-formed HTTP headers, and the
+     *            status must be an integer (i.e., the one
+     *            associated with the HTTP status code).
+     * @return array
+     */
     protected function parse($response)
     {
         $defaults = array(
-            'body'    => '',
-            'headers' => array('Content-Type: text/html; charset=utf-8'),
-            'status'  => 200
+            'body' => '',
+            'headers' => array(
+                'Content-Type: text/html; charset=utf-8'
+            ),
+            'status' => 200
         );
         if (is_array($response)) {
             $response += $defaults;
@@ -129,20 +132,21 @@ class Response
         return $response;
     }
 
-   /**
-    * Renders a response.
-    *
-    * @param mixed $response    The response to be rendered.  Can be an array
-    *                           containing 'body', 'headers', and/or 'status'
-    *                           keys, or a string which will be used as the
-    *                           body of the response.  Note that the headers
-    *                           must be well-formed HTTP headers, and the
-    *                           status must be an integer (i.e., the one
-    *                           associated with the HTTP status code).  The
-    *                           response body is chunked according to the
-    *                           buffer_size set in the constructor.
-    * @return void
-    */
+    /**
+     * Renders a response.
+     *
+     * @param mixed $response
+     *            The response to be rendered. Can be an array
+     *            containing 'body', 'headers', and/or 'status'
+     *            keys, or a string which will be used as the
+     *            body of the response. Note that the headers
+     *            must be well-formed HTTP headers, and the
+     *            status must be an integer (i.e., the one
+     *            associated with the HTTP status code). The
+     *            response body is chunked according to the
+     *            buffer_size set in the constructor.
+     * @return void
+     */
     public function render($response)
     {
         $response = $this->parse($response);
@@ -151,7 +155,7 @@ class Response
         foreach ($response['headers'] as $header) {
             header($header, false);
         }
-
+        
         $buffer_size = $this->config['buffer_size'];
         $length = strlen($response['body']);
         for ($i = 0; $i < $length; $i += $buffer_size) {
