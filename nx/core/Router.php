d485bfc07666bd97b1d7703715402c29eefdb068
Johannes Frandsen <localgod@heaven.dk>
2015-05-31 16:50:41 +0200

    Small tweak

--- nx/core/Router.php
+++ nx/core/Router.php
@@ -14,9 +14,9 @@ namespace nx\core;
 /**
  * The Router is used to handle url routing.
  *
- * @author    Nick Sinopoli <NSinopoli@gmail.com>
+ * @author Nick Sinopoli <NSinopoli@gmail.com>
  * @copyright 2011-2012 Nick Sinopoli
- * @license   http://opensource.org/licenses/BSD-3-Clause The BSD License
+ * @license http://opensource.org/licenses/BSD-3-Clause The BSD License
  */
 class Router
 {
@@ -24,24 +24,25 @@ class Router
     /**
      * Compiles the regex necessary to capture all match types within a route.
      *
-     * @param string $route    The route.
+     * @param string $route
+     *            The route.
      * @return string
      */
     protected function compileRegex($route)
     {
         $pattern = '`(/|\.|)\[([^:\]]*+)(?::([^:\]]*+))?\](\?|)`';
-
+        
         if (preg_match_all($pattern, $route, $matches, PREG_SET_ORDER)) {
             $match_types = array(
-                'i'  => '[0-9]++',
-                'a'  => '[0-9A-Za-z]++',
-                'h'  => '[0-9A-Fa-f]++',
-                '*'  => '.+?',
-                ''   => '[^/]++'
+                'i' => '[0-9]++',
+                'a' => '[0-9A-Za-z]++',
+                'h' => '[0-9A-Fa-f]++',
+                '*' => '.+?',
+                '' => '[^/]++'
             );
             foreach ($matches as $match) {
-                list($block, $pre, $type, $param, $optional) = $match;
-
+                list ($block, $pre, $type, $param, $optional) = $match;
+                
                 if (isset($match_types[$type])) {
                     $type = $match_types[$type];
                 }
@@ -51,7 +52,7 @@ class Router
                 if ($optional) {
                     $optional = '?';
                 }
-
+                
                 $replaced = "(?:{$pre}({$param}{$type})){$optional}";
                 $route = str_replace($block, $replaced, $route);
             }
@@ -70,10 +71,10 @@ class Router
      *
      * <code>
      * $routes = array(
-     *     array(
-     *         mixed $request_method, string $request_uri, callable $callback
-     *     ),
-     *     ...
+     * array(
+     * mixed $request_method, string $request_uri, callable $callback
+     * ),
+     * ...
      * );
      * </code>
      *
@@ -81,12 +82,12 @@ class Router
      *
      * <code>
      * $request_method can be a string ('GET', 'POST', 'PUT', 'DELETE'),
-     * or an array (e.g., array('GET, 'POST')).  Note that $request_method
+     * or an array (e.g., array('GET, 'POST')). Note that $request_method
      * is case-insensitive.
      * </code>
      *
      * <code>
-     * $request_uri is a string, with optional match types.  Valid match types
+     * $request_uri is a string, with optional match types. Valid match types
      * are as follows:
      *
      * [i] - integer
@@ -119,60 +120,59 @@ class Router
      *
      * Returns an array containing the following keys:
      *
-     * * 'params'   - The parameters collected from the matched uri
+     * * 'params' - The parameters collected from the matched uri
      * * 'callback' - The callback function pulled from the matched route
      *
-     * @param string $request_uri       The request uri.
-     * @param string $request_method    The request method.
-     * @param array $routes             The routes.
+     * @param string $requestUri
+     *            The request uri.
+     * @param string $requestMethod
+     *            The request method.
+     * @param array $routes
+     *            The routes.
      * @return array
      */
-    public function parse($request_uri, $request_method, $routes)
+    public function parse($requestUri, $requestMethod, $routes)
     {
         foreach ($routes as $route) {
-            list($method, $uri, $callback) = $route;
-
+            list ($method, $uri, $callback) = $route;
+            
             if (is_array($method)) {
                 $found = false;
                 foreach ($method as $value) {
-                    if (strcasecmp($request_method, $value) == 0) {
+                    if (strcasecmp($requestMethod, $value) == 0) {
                         $found = true;
                         break;
                     }
                 }
-                if (!$found) {
+                if (! $found) {
                     continue;
                 }
-            } elseif (strcasecmp($request_method, $method) != 0) {
+            } elseif (strcasecmp($requestMethod, $method) != 0) {
                 continue;
             }
-
+            
             if (is_null($uri) || $uri == '*') {
                 $params = array();
                 return compact('params', 'callback');
             }
-
+            
             $route_to_match = '';
             $len = strlen($uri);
-
-            for ($i = 0; $i < $len; $i++) {
+            
+            for ($i = 0; $i < $len; $i ++) {
                 $char = $uri[$i];
-                $is_regex = (
-                    $char == '[' || $char == '(' || $char == '.'
-                    || $char == '?' || $char == '+' || $char == '{'
-                );
+                $is_regex = ($char == '[' || $char == '(' || $char == '.' || $char == '?' || $char == '+' || $char == '{');
                 if ($is_regex) {
                     $route_to_match = $uri;
                     break;
-                } elseif (!isset($request_uri[$i]) || $char != $request_uri[$i]
-                ) {
+                } elseif (! isset($requestUri[$i]) || $char != $requestUri[$i]) {
                     continue 2;
                 }
                 $route_to_match .= $char;
             }
-
+            
             $regex = $this->compileRegex($route_to_match);
-            if (preg_match($regex, $request_uri, $params)) {
+            if (preg_match($regex, $requestUri, $params)) {
                 foreach ($params as $key => $arg) {
                     if (is_numeric($key)) {
                         unset($params[$key]);
@@ -182,7 +182,7 @@ class Router
             }
         }
         return array(
-            'params'   => null,
+            'params' => null,
             'callback' => null
         );
     }
