be3fe48fa664ad0693c59f8986a9b4067a8babe4
Johannes Skov Frandsen <jsf@greenoak.dk>
2025-04-02 11:43:03 +0000

    Added eslint and added some dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+]);
