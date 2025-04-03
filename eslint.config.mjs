af713f6993cc8517dd229c801db6cf898b8832d5
Johannes Skov Frandsen <jsf@greenoak.dk>
2025-04-03 07:55:03 +0000

    disabled eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,7 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { rules: {
+    "no-undef": "off"
+  }}
 ]);
